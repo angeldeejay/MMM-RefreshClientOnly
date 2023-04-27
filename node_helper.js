e9e9edab10c4b8500cc2a56f04211f3bac9265d1
Andres Vanegas <ukab72106@gmail.com>
2023-04-27 01:54:29 -0500

    Checking custom.css

--- node_helper.js
+++ node_helper.js
@@ -5,20 +5,39 @@
  * @license MIT
  */
 
-const NodeHelper = require("node_helper");
-const Log = require("../../js/logger.js");
 const { v4: uuid } = require("uuid");
+const cssPath = "../../css/custom.css";
+const fs = require("node:fs");
+const Log = require("../../js/logger.js");
+const NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
   uuid: null,
+  cssMtime: null,
   start: function () {
     Log.log("Starting MMM-RefreshClientOnly");
     this.uuid = uuid();
+    setInterval(() => this.checkCssMtime(), 1000);
+  },
+
+  checkCssMtime: function () {
+    if (fs.existsSync(cssPath)) {
+      const stats = fs.statSync(cssPath);
+      const lastModifiedTime = stats.mtime;
+      const savedModifiedTime = this.cssMtime;
+      if (lastModifiedTime !== savedModifiedTime) {
+        this.cssMtime = lastModifiedTime;
+        if (savedModifiedTime !== null) {
+          Log.info(`Updated custom stylesheet. Reloading`);
+          this.uuid = uuid();
+        }
+      }
+    }
   },
 
   socketNotificationReceived: function (notification, payload) {
     if (notification === "GET_UUID") {
       this.sendSocketNotification("UUID", this.uuid);
     }
-  }
+  },
 });
